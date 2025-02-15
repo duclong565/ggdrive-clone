3025e027ab7123081ab2a56235a17d83892dc138
duclong565 <nguyenduclong5a1@gmail.com>
2025-02-15 23:39:13 +0700

    upload files that not image only

--- src/app/drive-contents.tsx
+++ src/app/drive-contents.tsx
@@ -66,7 +66,7 @@ export default function DriveContents(props: {
           </ul>
         </div>
         <UploadButton
-          endpoint="imageUploader"
+          endpoint="fileUploader"
           onClientUploadComplete={() => {
             navigate.refresh();
           }}
